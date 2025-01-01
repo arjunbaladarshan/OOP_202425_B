522df68bdc1908edeccd85b1ce67aa260f68ab87
arjunbaladarshan <60723978+arjunbaladarshan@users.noreply.github.com>
2025-01-01 11:34:35 +0530

    looping statement

--- NewDemo.java
+++ NewDemo.java
@@ -1,5 +1,12 @@
 public class NewDemo{
     public static void main(String[] args){
-        System.out.println("Second Demo");
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(8-i>=j){
+                    System.out.print("("+i+"-"+j+")"+" ");
+                }
+            }
+            System.out.println("");
+        }
     }
 }
